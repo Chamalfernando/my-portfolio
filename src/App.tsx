73587b34419f69fd061f860846a37fd9af6ec734
Chamalfernando <chamaldeshitha2001@gmail.com>
2025-04-12 18:13:59 +0530

    [CHANGES] updated project tailwindcss with postcss and typescript.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="italic text-color-primary text-2xl font-bold">
+          This is a simple React app. Edit <code>src/App.tsx</code> and save to
+          reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
