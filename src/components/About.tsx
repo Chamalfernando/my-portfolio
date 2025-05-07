1584f216e9859241570c5342234ef1bf3f5325ee
Chamalfernando <chamaldeshitha2001@gmail.com>
2025-05-07 09:29:15 +0530

    [CHANGES]

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // src/components/About.tsx
 const About = () => (
   <section id="about" className="mt-5 py-8 px-6 max-w-4xl mx-auto">
-    <h3 className="text-2xl font-bold mb-4">About Me</h3>
+    <h3 className="text-2xl font-bold mb-4">About My Self</h3>
     <p>
       I'm a 3rd-year Computer Science undergraduate at UCSC. I've worked on
       several projects with Full Stack Development. I'm eager to apply what I
